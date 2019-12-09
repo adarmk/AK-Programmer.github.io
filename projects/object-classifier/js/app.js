00fc38f2176fd07c2c7f50f410633b98bcdf1bcf
Adar Kahiri <30850318+AK-Programmer@users.noreply.github.com>
2019-12-09 09:11:19 -0500

    Add files via upload

---
+++ projects/object-classifier/js/app.js
@@ -0,0 +1,37 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+document.getElementById('output').innerHTML = "Cat";
+
+
+/* DO ML SHIT HERE
+
+    1. Preprocess input from camera (update every 1000ms)
+    2. Put into model
+    3. Get output
+    4. Display output in 'output' variable. 
+*/
+
+
+
+
+cameraTrigger.onclick = function() { //Start camera and load model when button pressed
+    cameraStart(); 
+}; 
+
